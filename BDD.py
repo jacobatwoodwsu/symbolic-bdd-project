f8e62736faf63cd3f3e0f16ec47565461082ec85
jacobatwoodwsu <jacob.atwood@wsu.edu>
2025-11-09 08:25:28 -0800

    Update

--- BDD.py
+++ BDD.py
@@ -3,14 +3,13 @@ from pyeda.inter import *
 
 def equal_bits(a_bits, b_bits):
     assert len(a_bits) == len(b_bits)
-    terms = []
+    term = 1
     for a, b in zip(a_bits, b_bits):
         if isinstance(b, int):
-            terms.append(a if b == 1 else ~a)
+            term &= a if b == 1 else ~a
         else:
-            terms.append(~(a^b))
-    return And(*terms)
-
+            term &= ~(a^b)
+    return term
 
 def int_to_bits(num, width=5):
     return [(num >> i) & 1 for i in reversed(range(width))]
@@ -20,25 +19,38 @@ def make_R_expr(x,y):
     R_expr = []
     for i in range(32):
         for j in [(i+3) % 32, (i + 8) % 32]:
-            R_expr.append(And(equal_bits(x, int_to_bits(i)), equal_bits(y, int_to_bits(j))))
-    return Or(*R_expr)
+            R_expr |= equal_bits(x, int_to_bits(i)) & equal_bits(y, int_to_bits(j))
+    return R_expr
 
 def E_j(*bits, num):
     bitlist = int_to_bits(num, len(bits))
-    terms = []
+    term = 1
     for b, bit_var in zip(bitlist, bits):
-        terms.append(bit_var if b == 1 else ~bit_var)
-    return And(*terms)
+        term &= bit_var if b == 1 else ~bit_var
+    return term
 
 def build_prime(*x):
     primes = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
-    prime_exprs = [E_j(*x, num=j) for j in primes]
-    return Or(*prime_exprs)
+    prime_exprs = 0
+    for p in primes:
+        prime_exprs |= E_j(*x, num=p)
+    return prime_exprs
 
 def build_even(*y):
     even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18 , 20, 22, 24, 26, 28, 30]
-    even_exprs = [E_j(*y, num=j) for j in even]
-    return Or(*even_exprs)
+    even_exprs = 0
+    for e in even:
+        even_exprs |= E_j(*y, num=e)
+    return even_exprs
+
+def buildRR2star(x, y, z, RR2):
+    RR2star = RR2
+    prev = None
+    while RR2star != prev:
+        prev = RR2star
+        step = (RR2star.compose({y[i]: z[i] for i in range(len(z))}) & RR2.compose({x[i]: z[i] for i in range(len(z))})).smoothing(z)
+        RR2star |= step
+    return RR2star
 
 def num_to_dict(num, var_list):
     bits = [(num >> i) & 1 for i in reversed(range(len(var_list)))]
@@ -65,6 +77,12 @@ def testRR2(num1, num2):
     val1.update(val2)
     return bool(RR2.restrict(val1))
 
+def testRR2star(num1, num2):
+    val1 = num_to_dict(num1, x)
+    val2 = num_to_dict(num2, y)
+    val1.update(val2)
+    return bool(RR2STAR.restrict(val1))
+
 def testFunctions():
     print("Testing RR:\n")
     print(f"RR(27, 3); Epected: True; Actual: {testRR(27, 3)}\n")
@@ -80,7 +98,11 @@ def testFunctions():
 
     print("Testing RR2:\n")
     print(f"RR2(27, 6); Epected: True; Actual: {testRR2(27, 6)}\n")
-    print(f"RR2(27, 9); Epected: False; Actual: {testRR(27, 9)}\n")
+    print(f"RR2(27, 9); Epected: False; Actual: {testRR2(27, 9)}\n")
+
+    print("Testing RR2Star:\n")
+    print(f"RR2STAR(27, 6); Epected: True; Actual: {testRR2star(27, 6)}\n")
+    print(f"RR2STAR(27, 9); Epected: False; Actual: {testRR2star(27, 9)}\n")
 
 
 if __name__ == '__main__':
@@ -88,15 +110,13 @@ if __name__ == '__main__':
     y = bddvars('y', 5)
     z = bddvars('z', 5)
 
-    RR = make_R_expr(x,y)
-    PRIME = build_prime(*x)
-    EVEN = build_even(*y)
+    RR = expr2bdd(make_R_expr(x,y))
+    PRIME = expr2bdd(build_prime(*x))
+    EVEN = expr2bdd(build_even(*y))
 
-    #RR2 = (RR.compose({y[i]: z[i] for i in range(5)}) and RR.compose({x[i]: z[i] for i in range(5)})).smoothing(z)
+    RR2 = (RR.compose({y[i]: z[i] for i in range(5)}) & RR.compose({x[i]: z[i] for i in range(5)})).smoothing(z)
 
-    val = num_to_dict(7, x)
-    print(val)
-    print(list(val.keys())[0], type(list(val.keys())[0]))
+    RR2STAR= buildRR2star(x, y, z, RR2)
 
     testFunctions()
 
